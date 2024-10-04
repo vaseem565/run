7ec79e92cce540f65d32a14755d626d590632743
vaseem <syedvaseem211@gmail.com>
2024-10-04 07:07:35 +0530

    test

---
+++ src/Oct/exc_03102024/lab_001.java
@@ -0,0 +1,10 @@
+package Oct.exc_03102024;
+
+public class lab_001 {
+    public static void main(String[] args) {
+        System.out.println("helloe world)");
+        System.out.println("helloe world welcome v)");
+
+
+    }
+}
